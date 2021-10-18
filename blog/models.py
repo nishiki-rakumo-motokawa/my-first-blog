7131c34ea44acbe706d41be8d24c2c4daa45f6dd
Nishiki Motokawa <n.motokawa@rakumo.com>
2021-10-18 15:27:01 +0900

    Modified templates to display posts from database.

--- blog/models.py
+++ blog/models.py
@@ -2,7 +2,6 @@ from django.conf import settings
 from django.db import models
 from django.utils import timezone
 
-
 class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
@@ -15,4 +14,4 @@ class Post(models.Model):
         self.save()
 
     def __str__(self):
-        return self.title
+        return self.title        
